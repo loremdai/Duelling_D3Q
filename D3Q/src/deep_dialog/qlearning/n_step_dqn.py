d2c11fa94558185355f4646161e707e78b78b606
loremdai <etienn3dai@gmail.com>
2021-10-21 21:18:29 +0800

    update

--- D3Q/src/deep_dialog/qlearning/n_step_dqn.py
+++ D3Q/src/deep_dialog/qlearning/n_step_dqn.py
@@ -118,7 +118,7 @@ class nStepDQN(nn.Module):
         self.gamma = 0.9 ** self.n_step
         self.reg_l2 = 1e-3
         self.max_norm = 10.0
-        self.target_update_period = 50
+        self.target_update_period = 100
         lr = 0.003
 
         self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
