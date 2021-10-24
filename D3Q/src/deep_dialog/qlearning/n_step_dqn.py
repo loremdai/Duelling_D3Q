f0ca77db20bf2d6f21358cbe53f0d4fd98378f01
loremdai <etienn3dai@gmail.com>
2021-10-24 16:20:52 +0800

    update

--- D3Q/src/deep_dialog/qlearning/n_step_dqn.py
+++ D3Q/src/deep_dialog/qlearning/n_step_dqn.py
@@ -119,7 +119,7 @@ class nStepDQN(nn.Module):
         self.reg_l2 = 1e-3
         self.max_norm = 10.0
         self.target_update_period = 100
-        lr = 0.007
+        lr = 0.002
 
         self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
 
@@ -127,9 +127,6 @@ class nStepDQN(nn.Module):
     def update_network(self):
         # update target network
         self.target_model.load_state_dict(self.model.state_dict())
-        # NoisyNet settings: (reset noise)
-        self.model.reset_noise()
-        self.target_model.reset_noise()
 
     def Variable(self, x):
         return Variable(x, requires_grad=False).to(device)
@@ -158,6 +155,9 @@ class nStepDQN(nn.Module):
         loss.backward()
         clip_grad_norm_(self.model.parameters(), self.max_norm)
         self.optimizer.step()
+        # NoisyNet settings: (reset noise)
+        self.model.reset_noise()
+        self.target_model.reset_noise()
 
     def predict(self, inputs):  # 输入是representation，一个numpy.hstack的矩阵
         inputs = self.Variable(torch.from_numpy(inputs).float())
