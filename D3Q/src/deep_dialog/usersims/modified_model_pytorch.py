2589d26a2ef21aa646f7a2b4ecd6d1cbab112014
loremdai <etienn3dai@gmail.com>
2021-10-25 13:42:34 +0800

    update

--- D3Q/src/deep_dialog/usersims/modified_model_pytorch.py
+++ D3Q/src/deep_dialog/usersims/modified_model_pytorch.py
@@ -65,7 +65,7 @@ class SimulatorModel(nn.Module):
 
         # hyper parameters
         self.max_norm = 1  # 梯度裁剪参数
-        lr = 0.002
+        lr = 0.003
 
         # optimizer
         self.optimizer = optim.Adam(self.parameters(), lr=lr)
