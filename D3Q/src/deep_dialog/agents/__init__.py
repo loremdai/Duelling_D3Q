30bc94eb75f5e4ecc4a424bbbbf85b897e529479
loremdai <etienn3dai@gmail.com>
2021-12-27 14:04:02 +0800

    update

--- D3Q/src/deep_dialog/agents/__init__.py
+++ D3Q/src/deep_dialog/agents/__init__.py
@@ -1,9 +1,9 @@
 from .agent_cmd import *
 from .agent_baselines import *
 from .agent_dqn import *
-from .agent_categorical import *
+from .agent_Categorical import *
 
-from .agent_nstep import *
+from .agent_NStep import *
 
 from .agent_NoNStep import *
 from .agent_NoNoisyNet import *
