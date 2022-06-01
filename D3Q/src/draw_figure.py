10d3b4737b99592cbc74f8ead414c481f47559fa
loremdai <etienn3dai@gmail.com>
2022-06-01 16:59:26 +0800

    update

--- D3Q/src/draw_figure.py
+++ D3Q/src/draw_figure.py
@@ -4,6 +4,8 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 import numpy as np
 
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 
 matplotlib.use('Agg')
 
