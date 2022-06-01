67841bb4f37ba6b12a395aca3578529d23d54315
loremdai <etienn3dai@gmail.com>
2022-06-01 16:45:42 +0800

    update

--- D3Q/src/draw_figure.py
+++ D3Q/src/draw_figure.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 import numpy as np
 
-plt.rcParams['font.sans-serif']=['Songti'] #显示中文标签
+plt.rcParams['font.sans-serif']=['Microsoft YaHei'] #显示中文标签
 plt.rcParams['axes.unicode_minus']=False   #这两行需要手动设置
 
 matplotlib.use('Agg')
